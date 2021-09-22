985dd1aa1ec94406182587d45396892d5a20cd4a
Mike <mike.sierra.7950@gmail.com>
2021-09-22 13:44:16 -0400

    Added script for deploy

--- provisionResources.ps1
+++ provisionResources.ps1
@@ -6,9 +6,18 @@ $vmSize = "Standard_B2s"
 $vmImage = $(az vm image list --query "[? contains(urn, 'Ubuntu')] | [0].urn" -o tsv)
 $vmAdminUsername = "student"
 $vmAdminPassword = "LaunchCode-@zure1"
-$kvName = "$studentName-lc0922-ps-kv-3"
+$kvName = "$studentName-lc0922-ps-kv-4"
 $kvSecretName = "ConnectionStrings--Default"
 $kvSecretValue = "server=localhost;port=3306;database=coding_events;user=coding_events;password=launchcode"
+# TODO: enter your GitHub user name
+$github_username = "Sanchez2047"
+
+# TODO: enter the name of your project branch that has your updated code
+$solution_branch = "3-aadb2c"
+
+# api
+$api_service_user = "api-user"
+$api_working_dir = "/opt/coding-events-api"
 
 az configure --default location=eastus
 
@@ -34,6 +43,105 @@ az keyvault secret set --vault-name $kvName --description 'connection string' --
 
 # TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
 az keyvault set-policy --name $kvName --object-id $vmId --secret-permissions list get
+
+# Create a new deliver-deploy script
+rm deliver-deploy.sh
+
+New-Item -Name "deliver-deploy.sh" -ItemType "file" -Value "
+#! /usr/bin/env bash
+
+set -ex
+
+# -- env vars --
+
+# for cloning in delivery
+
+# needed to use dotnet from within RunCommand
+export HOME=/home/student
+export DOTNET_CLI_HOME=/home/student
+
+# -- end env vars --
+
+# -- set up API service --
+
+# create API service user and dirs
+useradd -M '$api_service_user' -N
+mkdir '$api_working_dir'
+
+chmod 700 /opt/coding-events-api/
+chown $api_service_user /opt/coding-events-api/
+
+# generate API unit file
+cat << EOF > /etc/systemd/system/coding-events-api.service
+[Unit]
+Description=Coding Events API
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+User=$api_service_user
+WorkingDirectory=$api_working_dir
+ExecStart=/usr/bin/dotnet ${api_working_dir}/CodingEventsAPI.dll
+Restart=always
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=coding-events-api
+Environment=ASPNETCORE_ENVIRONMENT=Production
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+Environment=DOTNET_HOME=$api_working_dir
+EOF
+
+# -- end setup API service --
+
+# -- deliver --
+
+# deliver source code
+
+git clone https://github.com/$github_username/coding-events-api /tmp/coding-events-api
+
+cd /tmp/coding-events-api/CodingEventsAPI
+
+# checkout branch that has the appsettings.json we need to connect to the KV
+git checkout $solution_branch
+
+cat << EOF > /tmp/coding-events-api/CodingEventsAPI/appsettings.json
+{
+  'Logging': {
+    'LogLevel': {
+      'Default': 'Information',
+      'Microsoft': 'Warning',
+      'Microsoft.Hosting.Lifetime': 'Information'
+    }
+  },
+  'AllowedHosts': '*',
+  'ServerOrigin': '$vmIP',
+  'KeyVaultName': '$kvName',
+  'JWTOptions': {
+    'Audience': 'dacff9ec-c689-43e5-b72c-5b037acc87d8',
+    'MetadataAddress': 'https://mikecolton0915tenant.b2clogin.com/MikeColton0915tenant.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_susi-flow',
+    'RequireHttpsMetadata': true,
+    'TokenValidationParameters': {
+      'ValidateIssuer': true,
+      'ValidateAudience': true,
+      'ValidateLifetime': true,
+      'ValidateIssuerSigningKey': true
+    }
+  }
+}
+EOF
+
+
+dotnet publish -c Release -r linux-x64 -o '$api_working_dir'
+
+# -- end deliver --
+
+# -- deploy --
+
+# start API service
+service coding-events-api start
+
+# -- end deploy --"
 
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/1configure-vm.sh
 
